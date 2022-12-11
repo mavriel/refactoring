5ed95912880ab3ff9f14ae91bef55e8a02a3b4fc
mavriel <mavriel@gmail.com>
2022-12-11 16:30:19 +0900

    make PerformanceCalculator.js

--- createStatementData.js
+++ createStatementData.js
@@ -1,4 +1,5 @@
 const _ = require("ramda");
+const PerformanceCalculator = require("./PerformanceCalculator");
 
 function createStatementData(invoice, plays) {
   const statementData = {};
@@ -8,41 +9,12 @@ function createStatementData(invoice, plays) {
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance, plays[aPerformance.playID]);
     const result = { ...aPerformance };
-    result.play = plays[aPerformance.playID];
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
-
-    function amountFor(aPerformance) {
-      let result = 0;
-      switch (aPerformance.play.type) {
-        case "tragedy":
-          result = 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-        case "comedy":
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-      }
-      return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-      let result = Math.max(aPerformance.audience - 30, 0);
-      if ("comedy" === aPerformance.play.type) {
-        result += Math.floor(aPerformance.audience / 5);
-      }
-      return result;
-    }
   }
 
   function totalAmount(data) {
