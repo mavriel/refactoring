d7857ac77e85e237b4db605c4334d17c6d3fb65d
mavriel <mavriel@gmail.com>
2022-12-11 15:05:32 +0900

    extract totalVolumeCredits

--- statement.js
+++ statement.js
@@ -1,16 +1,12 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
-
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
   function amountFor(aPerformance) {
     let result = 0;
@@ -32,6 +28,13 @@ function statement(invoice, plays) {
         throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
     return result;
+  }
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
 
   function volumeCreditsFor(aPerformance) {
