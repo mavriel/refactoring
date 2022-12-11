0111f0f81d4f5995276f4cda05dbbb0acbfe8150
mavriel <mavriel@gmail.com>
2022-12-11 16:13:12 +0900

    add htmlStatement

--- statement.js
+++ statement.js
@@ -15,6 +15,23 @@ function renderPlainText(data) {
   return result;
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>`;
+  result += "<table>";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${perf.play.audience}석</td><td>${usd(perf.amount)}</td></tr>`;
+  }
+  result += "</table>";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>`;
+  return result;
+}
+
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -23,4 +40,7 @@ function usd(aNumber) {
   }).format(aNumber / 100);
 }
 
-module.exports = statement;
+module.exports = {
+  statement,
+  htmlStatement,
+};
--- statement.test.js
+++ statement.test.js
@@ -1,9 +1,12 @@
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
-const statement = require("./statement");
+const { statement, htmlStatement } = require("./statement");
 
 test("invoices", () => {
   expect(statement(invoices[0], plays)).toEqual(
     "청구 내역 (고객명: BigCo)\n Hamlet: $650.00 (55석)\n As You Like It: $580.00 (35석)\n Othello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트: 47점\n"
+  );
+  expect(htmlStatement(invoices[0], plays)).toEqual(
+    "<h1>청구 내역 (고객명: BigCo)</h1><table><tr><th>연극</th><th>좌석 수</th><th>금액</th></tr><tr><td>Hamlet</td><td>undefined석</td><td>$650.00</td></tr><tr><td>As You Like It</td><td>undefined석</td><td>$580.00</td></tr><tr><td>Othello</td><td>undefined석</td><td>$500.00</td></tr></table><p>총액: <em>$1,730.00</em></p><p>적립 포인트: <em>47</em>점</p>"
   );
 });
