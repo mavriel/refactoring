e9fd0b682e7cfdc948ae5ea0d04ce39cbb3262a3
mavriel <mavriel@gmail.com>
2022-12-24 06:49:11 +0900

    4장완료

--- 4장/Province.test.js
+++ 4장/Province.test.js
@@ -1,11 +1,51 @@
 const Province = require("./Province");
 const data = require("./testData");
-const assert = require("assert");
 
 describe("province", () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(data);
+  });
+
   it("shortfall", () => {
-    const asia = new Province(data);
     expect(asia.shortfall).toEqual(5);
-    assert.equal(asia.shortfall, 5);
+  });
+  it("profit", () => {
+    expect(asia.profit).toEqual(230);
+  });
+  it("change production", () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toEqual(-6);
+    expect(asia.profit).toEqual(292);
+  });
+  it("zero demand", () => {
+    asia.demand = 0;
+    expect(asia.shortfall).toEqual(-25);
+    expect(asia.profit).toEqual(0);
+  });
+  it("negative demand", () => {
+    asia.demand = -1;
+    expect(asia.shortfall).toEqual(-26);
+    expect(asia.profit).toEqual(-10);
+  });
+  it("empty string demand", () => {
+    asia.demand = "";
+    expect(asia.shortfall).toBeNaN();
+    expect(asia.profit).toBeNaN();
+  });
+});
+
+describe("no producers", () => {
+  let noProducers;
+  beforeEach(() => (noProducers = new Province({ name: "No producers", producers: [], demand: 30, price: 20 })));
+
+  it("shortfall", () => expect(noProducers.shortfall).toEqual(30));
+  it("profit", () => expect(noProducers.profit).toEqual(0));
+});
+
+describe("string for producers", () => {
+  it("", () => {
+    const prov = new Province({ name: "No producers", producers: "", demand: 30, price: 20 });
+    expect(prov.shortfall).toEqual(0);
   });
 });
